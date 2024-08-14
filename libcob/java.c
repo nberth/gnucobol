0c60f7d22db705d3aeb7742353305c1f714f167b
Nicolas Berthier <nicolas.berthier@ocamlpro.com>
2024-08-14 12:52:09 +0200

    Dynamic loading of JNI, with preliminary tests

--- libcob/java.c
+++ libcob/java.c
@@ -1,15 +1,19 @@
 /*
    Copyright (C) 2024 Free Software Foundation, Inc.
-   Written by Vedant Tewari
+   Written by Vedant Tewari, Nicolas Berthier,
+
    This file is part of GnuCOBOL.
+
    The GnuCOBOL runtime library is free software: you can redistribute it
    and/or modify it under the terms of the GNU Lesser General Public License
    as published by the Free Software Foundation, either version 3 of the
    License, or (at your option) any later version.
+
    GnuCOBOL is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
+
    You should have received a copy of the GNU Lesser General Public License
    along with GnuCOBOL.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -19,27 +23,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <jni.h>
 
 /* Force symbol exports */
 #define	COB_LIB_EXPIMP
 #include "libcob.h"
 #include "coblocal.h"
-
-#if WITH_JNI
-
-#include <jni.h>
+#include "java.h"
 
 /* Declarations */
 static JavaVM *jvm = NULL;
 static JNIEnv *env = NULL;
 
 typedef struct __cob_java_static_method {
-	jclass cls;
-	jmethodID mid;
+	jclass		cls;
+	jmethodID	mid;
 } cob_java_handle;
 
 static int			/* non-zero means there's an error */
-cob_java_initialize() {
+jvm_load (void) {
     /* JDK/JRE 6 VM initialization arguments */
     JavaVMInitArgs args;
     JavaVMOption* options;
@@ -61,96 +63,69 @@ cob_java_initialize() {
     return JNI_CreateJavaVM(&jvm, (void**)&env, &args);
 }
 
+static
 cob_java_handle*
-cob_resolve_java (const char *class_name,
-		  const char* method_name,
-		  const char *type_signature) {
-  jclass cls;
-  jmethodID mid;
-  cob_java_handle *handle;
+resolve_java (const char		*class_name,
+	      const char		*method_name,
+	      const char		*method_signature) {
+	jclass cls;
+	jmethodID mid;
+	cob_java_handle *handle;
 
-    /* Resolving Reference to Java Class and Static Method */
-    if (jvm == NULL) {
-      if (cob_java_initialize ()) {
-	return NULL;
-      }
-    }
+	char *jni_class_name = strdup(class_name);
+	for (char *p = jni_class_name; *p; ++p) {
+		if (*p == '.') {
+			*p = '_';
+		}
+	}
 
-    char *jni_class_name = strdup(class_name);
-    for (char *p = jni_class_name; *p; ++p) {
-        if (*p == '.') {
-            *p = '_';
-        }
-    }
-    
-    cls = (*env)->FindClass(env, jni_class_name);
-    free(jni_class_name);
-    if (!cls) {
-        return NULL;
-    }
+	cls = (*env)->FindClass(env, jni_class_name);
+	free(jni_class_name);
+	if (!cls) {
+		return NULL;
+	}
 
-    mid = (*env)->GetStaticMethodID(env, cls, method_name, type_signature);
-    if (!mid) {
-        (*env)->DeleteLocalRef(env, cls);
-        return NULL;
-    }
+	mid = (*env)->GetStaticMethodID(env, cls, method_name, method_signature);
+	if (!mid) {
+		(*env)->DeleteLocalRef(env, cls);
+		return NULL;
+	}
 
-    handle = (cob_java_handle*)cob_malloc(sizeof(cob_java_handle));
-    if (!handle) {
-        (*env)->DeleteLocalRef(env, cls);
-        return NULL;
-    }
+	handle = (cob_java_handle*)cob_malloc(sizeof(cob_java_handle));
+	if (!handle) {
+		(*env)->DeleteLocalRef(env, cls);
+		return NULL;
+	}
 
-    handle->cls = (*env)->NewGlobalRef(env, cls);
-    handle->mid = mid;
-    (*env)->DeleteLocalRef(env, cls);
+	handle->cls = (*env)->NewGlobalRef(env, cls);
+	handle->mid = mid;
+	(*env)->DeleteLocalRef(env, cls);
 
-    return handle;
+	return handle;
 }
 
-#else	/* !WITH_JNI */
-
-typedef struct __cob_java_static_method {
-	void *;
-	void *;
-} cob_java_handle;
-
-cob_java_handle *
-cob_resolve_java (const char *class_name,
-		  const char* method_name,
-		  const char *type_signature)
+static void
+call_java (const cob_java_handle *method_handle)
 {
-	return NULL;
-}
-
-#endif
-
-void
-cob_call_java (const cob_java_handle *method_handle)
-{
-#if WITH_JNI
 	if (method_handle == NULL) {
 		return;
 	}
-	(*env)->CallStaticVoidMethod(env, method_handle->cls,
-					     method_handle->mid, NULL);
+	(*env)->CallStaticVoidMethod(env,
+				     method_handle->cls,
+				     method_handle->mid, NULL);
 	if ((*env)->ExceptionCheck(env)) {
 		(*env)->ExceptionDescribe(env);
 		(*env)->ExceptionClear(env);
 	}
-#else
-	static int first_java = 1;
+}
 
-	COB_UNUSED (method_handle);
-
-	if (first_java) {
-		first_java = 0;
-		cob_runtime_warning (_("runtime is not configured to support %s"),
-			"JNI");
+/* Entry-point for the module: initializes a Java API structure. */
+int
+cob_jni_init (cob_java_api *api) {
+	if (jvm_load ()) {
+		return 1;	/* Unable to initialize/load the JVM */
 	}
-#if 0	/* TODO: if there is a register in Java-interop, then set it */
-	set_json_exception (JSON_INTERNAL_ERROR);
-#endif
-	cob_add_exception (COB_EC_IMP_FEATURE_DISABLED);
-#endif
+	api->cob_resolve = resolve_java;
+	api->cob_call = call_java;
+	return 0;
 }
